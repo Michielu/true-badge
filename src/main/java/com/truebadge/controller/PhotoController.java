b093fdc6d1dacdf5602ab1ceeb6756f3edc974ba
Michielu Menning <mich.menning@gmail.com>
2019-09-21 15:45:16 -0500

    Upload image to mongodb

--- src/main/java/com/truebadge/controller/PhotoController.java
+++ src/main/java/com/truebadge/controller/PhotoController.java
@@ -2,13 +2,18 @@ package com.truebadge.controller;
 
 import javax.validation.Valid;
 
+import org.bson.BsonBinarySubType;
+import org.bson.types.Binary;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.truebadge.models.Photo;
 import com.truebadge.repositories.PhotoRepository;
@@ -29,6 +34,21 @@ public class PhotoController {
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public Photo getPhotoById(@PathVariable("id") ObjectId id) {
 		return repository.findBy_id(id);
+	}
+	
+	@RequestMapping(value="/upload" ,method = RequestMethod.POST)
+	public String singleFileUpload(MultipartFile multipart, @RequestParam("title") String title) {
+	    try {
+	        Photo demoPhoto = new Photo();
+	        demoPhoto.setTitle(title);
+	        demoPhoto.setImage(new Binary(BsonBinarySubType.BINARY, multipart.getBytes()));
+	        repository.save(demoPhoto);
+	        System.out.println(demoPhoto);
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	        return "failure";
+	    }
+	    return "success";
 	}
 
 }
--- src/main/java/com/truebadge/models/Photo.java
+++ src/main/java/com/truebadge/models/Photo.java
@@ -4,39 +4,54 @@ import org.bson.types.Binary;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
 
+//@Document - applied at the class level to indicate this class is a candidate for mapping to the database. 
+//You can specify the name of the collection where the database will be stored.
 @Document(collection = "photos")
 public class Photo {
-	    @Id
-	    private ObjectId _id;
-	    	
-	    private String title;
-	    private Binary image;
-	    
-	    public Photo() {}
-	    
-	    public Photo(ObjectId id, String title, Binary image) {
-			super();
-			this._id = id;
-			this.title = title;
-			this.image = image;
-		}
+	//@Id applied at the field level to mark the field used for identiy purpose
+	@Id
+	private ObjectId _id;
 
+	//This is for testing
+	private String title;
+	private Binary image;
 
-		// ObjectId needs to be converted to string
-	    public String get_id() { return _id.toHexString(); }
-	    public void set_id(ObjectId _id) { this._id = _id; }
+	public Photo() {
+	}
 
-		public String getTitle() {return title;	}
-		public void setTitle(String title) {
-			this.title = title;
-		}
+	public Photo(ObjectId id, String title, Binary image) {
+		super();
+		this._id = id;
+		this.title = title;
+		this.image = image;
+	}
 
-		public Binary getImage() {
-			return image;
-		}
-		public void setImage(Binary image) {
-			this.image = image;
-		}
-	  
+	//Do I need these? 
+	// ObjectId needs to be converted to string
+	public String get_id() {
+		return _id.toHexString();
+	}
+
+	public void set_id(ObjectId _id) {
+		this._id = _id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Binary getImage() {
+		return image;
+	}
+
+	public void setImage(Binary image) {
+		this.image = image;
+	}
+
 }
